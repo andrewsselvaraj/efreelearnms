2e5b02a6c8751babe69b13da0ffde4862b031c22
shiju <shiju@LAPTOP-QFQM3362>
2020-06-28 10:37:35 +0530

    Base Version

---
+++ src/main/java/com/efreelearn/efreelearnms/EfreelearnmsApplication.java
@@ -0,0 +1,13 @@
+package com.efreelearn.efreelearnms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfreelearnmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfreelearnmsApplication.class, args);
+	}
+
+}
