26051c8d9a30d25d5b59f2cafe159c7ddda49573
shiju <shiju@LAPTOP-QFQM3362>
2020-06-28 14:26:37 +0530

    Zuul Proxy

--- src/main/java/com/efreelearn/efreelearnms/EfreelearnmsApplication.java
+++ src/main/java/com/efreelearn/efreelearnms/EfreelearnmsApplication.java
@@ -2,12 +2,21 @@ package com.efreelearn.efreelearnms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.efreelearn.routingandfilteringgateway.filters.pre.SimpleFilter;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class EfreelearnmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EfreelearnmsApplication.class, args);
 	}
+	@Bean
+	  public SimpleFilter simpleFilter() {
+	    return new SimpleFilter();
+	  }
 
 }
